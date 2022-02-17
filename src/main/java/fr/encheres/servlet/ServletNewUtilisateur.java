0e114eedfc0b7981aab1278807d158bb76ec8e57
Sandra <sbarteau@gmail.com>
2022-02-17 11:18:11 +0100

    Servlet newUtilisateur

--- src/main/java/fr/encheres/servlet/ServletNewUtilisateur.java
+++ src/main/java/fr/encheres/servlet/ServletNewUtilisateur.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.encheres.bll.UtilisateurManager;
+import fr.encheres.bo.Utilisateur;
+import fr.encheres.exception.BusinessException;
+
 /**
  * Servlet implementation class ServletNewUtilisateur
  */
@@ -23,17 +27,47 @@ public class ServletNewUtilisateur extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String pseudo;
+		String nom;
+		String prenom;
+		String email;
+		String telephone;
+		String rue;
+		String codePostal;
+		String ville;
+		String motDePasse;
+		Utilisateur newUtilisateur = null;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+			pseudo = request.getParameter("pseudo");
+			nom =request.getParameter("nom");
+			prenom = request.getParameter("prenom")	;
+			email = request.getParameter("email");
+			telephone = request.getParameter("telephone");
+			rue = request.getParameter("rue");
+			codePostal = request.getParameter("codePostal");
+			ville = request.getParameter("ville");
+			motDePasse = request.getParameter("motDePasse");
+			
+			newUtilisateur.setPseudo("pseudo");
+			newUtilisateur.setNom("nom");
+			newUtilisateur.setPrenom("prenom");
+			newUtilisateur.setEmail("email");
+			newUtilisateur.setTelephone("telephone");
+			newUtilisateur.setRue("rue");
+			newUtilisateur.setCodePostal("codePostal");
+			newUtilisateur.setVille("ville");
+			newUtilisateur.setMotDePasse("motDePasse");
+			
+			UtilisateurManager utilisateurManager = new UtilisateurManager();
+			
+			try {
+				utilisateurManager.ajouterUtilisateur(newUtilisateur);
+			} catch (BusinessException e) {
+				
+				e.printStackTrace();
+			}
+	
+
 		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/NewUtilisateur.jsp").forward(request, response);
 	}
 
