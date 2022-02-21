210388f9ae12c9bc04cc64ba17da6569d309ccc5
Lydia <Utilisateur@LAPTOP-L4HQ889S>
2022-02-21 18:23:56 +0100

    gestion des erreurs

---
+++ src/main/java/fr/encheres/bll/CodesResultatBLL.java
@@ -0,0 +1,11 @@
+package fr.encheres.bll;
+
+/**
+ * Les codes disponibles sont entre 20000 et 29999
+ */
+public abstract class CodesResultatBLL {
+	/**
+	 * Erreur sur l'écriture du pseudo utilisateur
+	 */
+	public static final int REGLE_UTILISATEUR_PSEUDO_ERREUR=20000;
+}
---
+++ src/main/java/fr/encheres/servlet/CodesResultatServlet.java
@@ -0,0 +1,8 @@
+package fr.encheres.servlet;
+
+/**
+ * Les codes disponibles sont entre 30000 et 39999
+ */
+public abstract class CodesResultatServlet {
+
+}
