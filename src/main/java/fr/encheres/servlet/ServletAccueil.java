21ec68b18ad930dd4d66ce61f9a89b33d9c814c0
Sandra <sbarteau@gmail.com>
2022-02-23 14:03:14 +0100

    page accueil

--- src/main/java/fr/encheres/servlet/ServletAccueil.java
+++ src/main/java/fr/encheres/servlet/ServletAccueil.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletAccueil
  */
-@WebServlet("/")
+@WebServlet("/ServletAccueil")
 public class ServletAccueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
